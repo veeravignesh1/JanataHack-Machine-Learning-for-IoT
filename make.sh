eaadebe6d89a1853ba2cb1aac603a212cdfc9b77
veeravignesh1 <kveeravignesh1@gmail.com>
2020-06-02 19:15:50 +0530

    Adding new make.sh

---
+++ make.sh
@@ -0,0 +1,9 @@
+mkdir dataset dataset/train dataset/test models notebooks output src utils
+touch dataset/train/README.md
+touch dataset/test/README.md
+touch models/README.md
+touch notebooks/README.md
+touch output/README.md
+touch src/README.md
+touch utils/README.md
+
